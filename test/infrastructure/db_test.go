8d8e6b1214a0d6d98e2a051c3d34b7bef9c1fae8
otiai10 <otiai10@gmail.com>
2014-03-19 15:29:34 +0900

    Update test

--- test/infrastructure/db_test.go
+++ test/infrastructure/db_test.go
@@ -1,37 +1,33 @@
 package infra
 
-import "fmt"
+import (
+	. "github.com/r7kamura/gospel"
+	"testing"
+)
+
 import "animapi/infrastructure"
-import "testing"
 
 func TestGetDb(t *testing.T) {
-    db := infra.GetDB("test", "000")
+	Describe(t, "Db", func() {
+		It("can read sql カッコカリ", func() {
 
-    res := db.CreateAnimeTable()
-    fmt.Printf("%+v\n", res)
-    res = db.DropAnimeTable()
-    fmt.Printf("%+v\n", res)
-    res = db.CreateAnimeTable()
-    fmt.Printf("%+v\n", res)
+			db := infra.GetDB("test", "000")
 
-    res = db.InsertAnime()
-    res = db.InsertAnime()
-    res = db.InsertAnime()
-    res = db.InsertAnime()
-    fmt.Printf("%+v\n", res)
+			_ = db.CreateAnimeTable()
+			_ = db.DropAnimeTable()
+			_ = db.CreateAnimeTable()
 
-    rows := db.FindAllAnime()
-    for rows.Next() {
-        fmt.Println("あっっっったああああああああ")
-        var title string
-        var id    string
-        e := rows.Scan(&id, &title)
-        if e != nil {
-            fmt.Println(e)
-        }
-        fmt.Println(
-            title,
-            id,
-        )
-    }
+			_ = db.InsertAnime()
+
+			rows := db.FindAllAnime()
+			rows.Next()
+			var title string
+			var id string
+			e := rows.Scan(&id, &title)
+			if e != nil {
+				panic(e)
+			}
+			Expect(id).To(Equal, "1")
+		})
+	})
 }
