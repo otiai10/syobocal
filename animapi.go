d77c27ab593564598e237d8b125f0d8a58b734c1
otiai10 <otiai10@gmail.com>
2014-05-24 20:52:28 +0900

    Reconstruct directories hierarchy

--- animapi.go
+++ animapi.go
@@ -1,12 +1,6 @@
 package animapi
 
-import "github.com/otiai10/animapi/application"
 import "time"
-
-var (
-	LOCAL    = application.Get(application.SRC_LOCAL)
-	SYOBOCAL = application.Get(application.SRC_SYOBOCAL)
-)
 
 func Since(snc string) (dur time.Duration, e error) {
 	s := &since{val: snc}
--- animapi_test.go
+++ animapi_test.go
@@ -14,13 +14,6 @@ func assert(t *testing.T, actual interface{}, expected interface{}) {
 	fmt.Printf("Expected to be `%+v`, but actual `%+v`\n", expected, actual)
 	os.Exit(1)
 }
-func TestAnimapi_LOCAL(t *testing.T) {
-	assert(
-		t,
-		animapi.LOCAL.Greet(),
-		"Hi, I'm Local!",
-	)
-}
 func TestAnimapi_SYOBOCAL(t *testing.T) {
 	assert(
 		t,
