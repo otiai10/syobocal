02e5283309a4b3b517aa0b121a5e587b42c98b9b
otiai10 <otiai10@gmail.com>
2014-05-25 13:30:28 +0900

    Add dummy function

--- syobocal.go
+++ syobocal.go
@@ -1,10 +1,6 @@
 package animapi
 
-import "net/http"
 import "time"
-
-import "fmt"
-import "io/ioutil"
 
 type syobocal struct{}
 
@@ -13,16 +9,6 @@ var SYOBOCAL = syobocal{}
 func (s syobocal) Greet() string {
 	return "Hi, I'm Syobocal!"
 }
+func (s syobocal) FindPrograms(since time.Duration) {
 
-// {{{
-type Program struct {
-	Title string
-}
-
-// }}}
-func (s syobocal) FindPrograms(since time.Duration) /*[]Program*/ {
-	url := "http://cal.syoboi.jp/db.php?Command=TitleLookup&TID=*&LastUpdate=20140525_000000-"
-	resp, _ := http.Get(url)
-	buf, _ := ioutil.ReadAll(resp.Body)
-	fmt.Printf("んごー %s\n", string(buf))
 }
