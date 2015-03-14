a5762a2bded00a62bf8eda08951ad97b8c3426d7
otiai10 <otiai10@gmail.com>
2015-03-15 08:01:49 +0900

    Fix TitleLookupResponse

--- syobocal/api.go
+++ syobocal/api.go
@@ -7,6 +7,19 @@ import (
 	"time"
 )
 
+// Category しょぼかる的カテゴリ
+// 1 アニメ
+// 10 アニメ(終了/再放送)
+// 7 OVA
+// 5 アニメ関連
+// 4 特撮
+// 8 映画
+// 3 テレビ
+// 2 ラジオ
+// 6 メモ
+// 0 その他
+type Category int
+
 // Client is client API for syobocal.
 // @see https://sites.google.com/site/syobocal/spec/db-php
 type Client struct{}
--- syobocal/response.go
+++ syobocal/response.go
@@ -2,6 +2,7 @@ package syobocal
 
 import (
 	"encoding/xml"
+	"strconv"
 	"time"
 )
 
@@ -19,8 +20,24 @@ type TitleLookupResponse struct {
 
 // TitleItem ...
 type TitleItem struct {
-	TID        string     `xml:"TID"`
-	LastUpdate syoboiTime `xml:"LastUpdate"`
+	TID           string     `xml:"TID"`           // アニメタイトルID
+	LastUpdate    syoboiTime `xml:"LastUpdate"`    // 最近更新された時間
+	Title         string     `xml:"Title"`         // アニメタイトル
+	ShortTitle    string     `xml:"ShortTitle"`    // タイトルの省略形
+	TitleYomi     string     `xml:"TitleYomi"`     // タイトルの日本語読み
+	TitleEN       string     `xml:"TitleEN"`       // タイトルを英語にしたらどうなんの的なやつ（英語なければ""）
+	TitleFlag     int        `xml:"TitleFlag"`     // たぶん再放送フラグ的なやつ
+	Comment       string     `xml:"Comment"`       // コメント（？）なんかいろいろごちゃまぜ
+	Category      Category   `xml:"Cat"`           // カテゴリ
+	FirstYear     syoboiInt  `xml:"FirstYear"`     // 最初に放送された年
+	FirstMonth    syoboiInt  `xml:"FirstMonth"`    // 最初に放送された月
+	FirstEndYear  syoboiInt  `xml:"FirstEndYear"`  // 最初の放送が終わった年（未終了なら0）
+	FirstEndMonth syoboiInt  `xml:"FirstEndMonth"` // 最初の放送が終わった月（未終了なら0）
+	FirstChannel  string     `xml:"FirstCh"`       // 最初に放送された放送局
+	Keywords      string     `xml:"Keywords"`      // 検索キーワード. カンマ区切り
+	UserPoint     int        `xml:"UserPoint"`     // しょぼかるって投票できるらしいのでそのポイント
+	UserPointRank int        `xml:"UserPointRank"` // しょぼかるって投票できるらしいのでその結果ランキング
+	SubTitles     string     `xml:"SubTitles"`     // 各放映話のサブタイトル（改行区切り？）
 }
 
 type syoboiTime struct{ time.Time }
@@ -36,5 +53,21 @@ func (t *syoboiTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error 
 		return nil
 	}
 	*t = syoboiTime{parsed}
+	return nil
+}
+
+type syoboiInt struct{ int }
+
+func (i *syoboiInt) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var val string
+	if err := d.DecodeElement(&val, &start); err != nil {
+		return nil
+	}
+	parsed, err := strconv.Atoi(val)
+	if err != nil {
+		*i = syoboiInt{0}
+		return nil
+	}
+	*i = syoboiInt{parsed}
 	return nil
 }
